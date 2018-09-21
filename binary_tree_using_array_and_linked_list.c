1241eaa7a4641a39832823d6bf85e79023cbd88f
Aditya Verma <39986925+adityaverma998@users.noreply.github.com>
2018-09-21 23:52:10 +0530

    Update binary_tree_using_array_and_linked_list.c

--- binary_tree_using_array_and_linked_list.c
+++ binary_tree_using_array_and_linked_list.c
@@ -4,10 +4,15 @@ binary tree implementation using array and linked list
 
 */
 
-#include<stdio.h>
+
+  
+  #include<stdio.h>
 #include<stdlib.h>
+struct node * tt1=NULL;
 int a[1000];int st=0;
 int n;int max=0;
+struct node * q[1000]; int front=0; int rear =0;
+
 struct node {
     int data;
     struct node * left ;
@@ -71,9 +76,12 @@ int main(){
     inorder(root);
     printf("\n\n");int m;
     struct node * k = root;
+    
+    
     while(1){
-        printf("\nchoose\n1.calculate maximum\n2.exit\n3.search\n");
+        printf("\nchoose\n1.calculate maximum\n2.exit\n3.search\n4.level order traversal\n");
         int c;
+        tt1 = root;
         scanf("%d",&c);
         switch(c){
             case 1:
@@ -94,6 +102,28 @@ int main(){
                 st=0;
             }
             break;
+            case 4:
+            // level order traversal 
+            
+            printf("\n");
+            while(tt1){
+                printf("%d ",tt1->data);
+                if(tt1->left){
+                    q[++rear]=tt1->left;
+                }
+                if(tt1->right){
+                    q[++rear]=tt1->right;
+                }
+                tt1 = q[++front];
+            }
+            
+            front =0;rear=0;
+            
+            printf("\n");
+            
+            break;
+            
+            
             default:printf("\nchoose corret option\n");
         }
     }
