7ff974b20fd4216ac5fd219c5b5c5dcfb110e094
Aditya Verma <39986925+adityaverma998@users.noreply.github.com>
2018-09-26 00:06:03 +0530

    Create ancestors_in_bst.c

---
+++ ancestors_in_bst.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node * left;
+    struct node * right;
+};
+struct node * newnode(int d){
+    struct node * nn = (struct node *)malloc(sizeof(struct node));
+    nn->data = d;
+    nn->left = NULL;
+    nn->right = NULL;
+    return nn;
+}
+struct node * inserttree(struct node * r,int val){
+    if(!r){
+        return newnode(val);
+    }
+    if(val <= r->data){
+        r->left = inserttree(r->left,val);
+    }else{
+        r->right = inserttree(r->right,val);
+    }
+    return r;
+}
+int ancestors(struct node * r,int val){
+    if(!r){
+        return 0;
+    }
+    if(r->data == val){
+        return 1;
+    }
+    if(ancestors(r->left,val)||ancestors(r->right,val)){
+        printf("%d ",r->data);
+        return 1;
+    }
+    return 0;
+}
+int main(){
+    printf("\nenter n :");
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    printf("\nenter the values :");
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    struct node * root = NULL;
+    root = inserttree(root,a[0]);
+    for(int i=1;i<n;i++){
+        inserttree(root,a[i]);
+    }
+    printf("\nenter the value whose ancesters you want to see");
+    int k;
+    scanf("%d",&k);
+    ancestors(root,k);
+    return 0;
+}
