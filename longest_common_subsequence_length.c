187529f5c150c96aa312c75d255e7fd0ab8433f6
Aditya Verma <39986925+adityaverma998@users.noreply.github.com>
2018-10-14 14:10:20 +0530

    Update longest_common_subsequence_length.c

--- longest_common_subsequence_length.c
+++ longest_common_subsequence_length.c
@@ -17,18 +17,34 @@ void lcs(char a[],char b[],int len1,int len2){
     }
     for(int i=0;i<=len1;i++){
         for(int j=0;j<=len2;j++){
-            if(i==0 || j==0){
+            if(i==0||j==0){
                 lcs[i][j]=0;
             }
             if(i>0 && j>0 && a[i-1]!=b[j-1]){
                 lcs[i][j]=max(lcs[i-1][j],lcs[i][j-1]);
             }
             if(i>0 && j>0 && a[i-1]==b[j-1]){
-                lcs[i][j]=1+lcs[i-1][j-1];
+                lcs[i][j]=lcs[i-1][j-1]+1;
             }
         }
     }
-    printf("\nlcs is : %d \n",lcs[len1][len2]);
+    printf("\nlcs is of length : %d ",lcs[len1][len2]);
+    int index = lcs[len1][len2];
+    char string[index+1];
+    string[index]='\0';
+    int i = len1;int j = len2;
+    while( i>0 && j>0 ){
+        if(a[i-1] == b[j-1]){
+            string[index-1]=a[i-1];
+            i--;j--;index--;
+        }else if(lcs[i-1][j] > lcs[i][j-1]){
+            i--;
+        }else{
+            j--;
+        }
+    }
+    printf("\nlcs is : ");
+    printf("%s",string);
 }
 int main(){
     printf("\nenter first string : ");
