a31c4f779496251b03f2a77d2a661b2339cfa0c4
Aditya Verma <39986925+adityaverma998@users.noreply.github.com>
2018-12-19 20:09:33 +0530

    Update graph_adjcency_list.cpp

--- graph_adjcency_list.cpp
+++ graph_adjcency_list.cpp
@@ -1,42 +1,42 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
-void addedge(vector <int> v[],int i, int src , int des){
-    v[src].push_back(des);
-    v[des].push_back(src);
+void addedge(vector <int> v[],int s,int d){
+    v[s].push_back(d);
+    v[d].push_back(s);
 }
 
-void print(vector <int> v[],int ver){
-    for(int i=0; i < ver; i++){
-        cout << i <<"--";
-        for(int j = 0; j < v[i].size(); j++){
-            cout << v[i][j] <<" ";
+void print(vector <int> v[],int n){
+    cout<<"\nADJACENCY LIST FOR GRAPH\n";
+    for(int i=0;i<n;i++){
+        cout<<i<<"--";
+        for(int j=0;j<v[i].size();j++){
+            cout<<v[i][j]<<" ";
         }
-        cout << "\n";
+        cout<<"\n";
     }
 }
 
-int main()
-{
+int main(){
     int v;
-    cout<<"enter the number of vertices in the graph : "<<"\n";
+    cout <<"enter the number of vertices : "<<"\n";
     cin>>v;
-    vector <int> a[v];
-    cout<<"enter the number of edges you want to enter : "<<"\n"; 
-    int n;
-    cin>>n;
-    for(int i=0; i < n; i++){
-        cout<<"edge no "<<i+1<<" \n";
+    vector <int> x[v];
+    int e;
+    cout<<"\n"<<"enter number of edges ";
+    cin>>e;
+    for(int i=0;i<e;i++){
+        cout << "for edge no "<<i+1<<"\n ";
         cout<<"enter src";
         int src;
         cin>>src;
-        cout<<"enter des ";
+        cout<<"enter des";
         int des;
         cin>>des;
-        addedge(a,i,src,des);
+        addedge(x,src,des);
     }
-    cout<<"\n";
-    print(a,v);
+    print(x,v);
     return 0;
 }
